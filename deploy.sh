134bce954fed1756cc0961089ed6af908417cb1e
hady-awayda <hady.awayda@gmai.com>
2024-08-14 13:26:41 +0300

    Updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # Create network if it doesn't exist
-docker network create hackathon-network || true
+# docker network create hackathon-network || true
 
-# Stop existing containers and remove images
-# docker-compose -f docker-compose.app.yml down --rmi local
+# # Stop existing containers and remove images
+# # docker-compose -f docker-compose.app.yml down --rmi local
 
-# Prune unused Docker resources
-docker system prune -f
+# # Prune unused Docker resources
+# docker system prune -f
 
-# Check if MySQL container exists
-if [ ! "$(docker ps -q -f name=hackathon-mysql-dev)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=hackathon-mysql-dev)" ]; then
-        # cleanup
-        docker rm hackathon-mysql-dev
-    fi
-    # run your container
-    docker-compose -f docker-compose.db.yml up -d
-fi
+# # Check if MySQL container exists
+# if [ ! "$(docker ps -q -f name=hackathon-mysql-dev)" ]; then
+#     if [ "$(docker ps -aq -f status=exited -f name=hackathon-mysql-dev)" ]; then
+#         # cleanup
+#         docker rm hackathon-mysql-dev
+#     fi
+#     # run your container
+#     docker-compose -f docker-compose.db.yml up -d
+# fi
 
 # Always run the app container
 docker-compose -f docker-compose.app.yml up -d --build
 
 # Cleanup images
-docker image prune -f
+# docker image prune -f
